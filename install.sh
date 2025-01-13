5070f875e01adfa2008bbe65d1c4aa06c8598b87
Giacomo 'Mr. Wolf' Furlan <giacomo.furlan@fastwebnet.it>
2025-01-13 13:17:32 +0100

    spec file (WIP)

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ else
     install_prefix=${PREFIX}
 fi
 
+if [ -x "${CHROOT}" ]; then
+    chroot_path="${CHROOT%/}"
+else
+    chroot_path=""
+fi
+
 # Files to install
 lib_files_and_dirs=("arctis_manager.py" "arctis_manager_launcher.py" "arctis_manager")
 bin_files=("bin/arctis-manager")
@@ -15,65 +21,81 @@ desktop_file="ArctisManager.desktop"
 icon_file="arctis_manager/images/steelseries_logo.svg"
 
 # Install directories
-applications_dir="${install_prefix}/share/applications/"
-icons_dir="/usr/share/icons/hicolor/scalable/apps/"
-bin_dir="${install_prefix}/bin"
-lib_dir="${install_prefix}/lib/arctis-manager"
-udev_dir="/usr/lib/udev/rules.d/"
-systemd_dir="/usr/lib/systemd/user/"
+applications_dir="${chroot_path}${install_prefix}/share/applications/"
+icons_dir="${chroot_path}/usr/share/icons/hicolor/scalable/apps/"
+bin_dir="${chroot_path}${install_prefix}/bin"
+lib_dir="${chroot_path}${install_prefix}/lib/arctis-manager"
+udev_dir="${chroot_path}/usr/lib/udev/rules.d/"
+systemd_dir="${chroot_path}/usr/lib/systemd/user/"
+
+function superuserdo() {
+    if [ "${chroot_path}" != "" ]; then
+        $@
+    else
+        sudo $@
+    fi
+}
 
 function install() {
     echo "Installing Arctis Manager..."
 
-    sudo mkdir -p "${bin_dir}"
-    sudo mkdir -p "${lib_dir}"
-    sudo mkdir -p "${applications_dir}"
-    sudo mkdir -p "${icons_dir}"
+    superuserdo mkdir -p "${bin_dir}"
+    superuserdo mkdir -p "${lib_dir}"
+    superuserdo mkdir -p "${applications_dir}"
+    superuserdo mkdir -p "${icons_dir}"
 
     echo "Installing binaries in ${bin_dir}"
     for file in "${bin_files[@]}"; do
         dest_file="${bin_dir}/$(basename "${file}")"
-        sudo cp "${file}" "${dest_file}"
+        superuserdo cp "${file}" "${dest_file}"
 
         # Replace placeholders
-        sudo sed -i "s|{{LIBDIR}}|${lib_dir}|g" "${dest_file}"
+        superuserdo sed -i "s|{{LIBDIR}}|${lib_dir}|g" "${dest_file}"
     done
 
     echo "Installing desktop file in ${applications_dir}"
     dest_file="${applications_dir}/$(basename "${desktop_file}")"
-    sudo cp "${desktop_file}" "${dest_file}"
+    superuserdo cp "${desktop_file}" "${dest_file}"
     # Replace placeholders
-    sudo sed -i "s|{{LIBDIR}}|${lib_dir}|g" "${dest_file}"
+    superuserdo sed -i "s|{{LIBDIR}}|${lib_dir}|g" "${dest_file}"
 
     echo "Installing icon file in ${icons_dir}"
     dest_file="${icons_dir}/arctis_manager.svg"
-    sudo cp "${icon_file}" "${dest_file}"
+    superuserdo cp "${icon_file}" "${dest_file}"
 
     echo "Installing application data in ${lib_dir}"
     for file in "${lib_files_and_dirs[@]}"; do
         if [ -f "${file}" ]; then
-            sudo cp "${file}" "${lib_dir}/"
+            superuserdo cp "${file}" "${lib_dir}/"
         elif [ -d "${file}" ]; then
-            sudo cp -r "${file}" "${lib_dir}/"
+            superuserdo cp -r "${file}" "${lib_dir}/"
         fi
     done
 
-    # Note: using sudo with pip to install modules
+    # Note: using superuserdo with pip to install modules
     # It is harmless, as it will target a custom folder
     echo "Installing python dependencies in ${lib_dir}"
-    sudo python3 -m pip install --upgrade --quiet --root-user-action=ignore -r requirements.txt --target "${lib_dir}"
+    superuserdo python3 -m pip install --upgrade --quiet --root-user-action=ignore -r requirements.txt --target "${lib_dir}"
 
     # Udev rules
     echo "Installing udev rules."
-    sudo cp "${udev_rules_file}" "${udev_dir}"
-    sudo udevadm control --reload
-    sudo udevadm trigger
+    superuserdo mkdir -p "${udev_dir}"
+    superuserdo cp "${udev_rules_file}" "${udev_dir}"
+    if [ "${chroot_path}" == "" ]; then
+        superuserdo udevadm control --reload
+        superuserdo udevadm trigger
+    fi
 
     # SystemD service
     echo "Installing and enabling systemd user service."
-    systemctl --user disable --now "$(basename ${systemd_service_file})" 2>/dev/null
-    sudo cp "${systemd_service_file}" "${systemd_dir}"
-    systemctl --user enable --now "$(basename ${systemd_service_file})"
+    if [ "${chroot_path}" == "" ]; then
+        systemctl --user disable --now "$(basename ${systemd_service_file})" 2>/dev/null
+    fi
+    superuserdo mkdir -p "${systemd_dir}"
+    superuserdo cp "${systemd_service_file}" "${systemd_dir}"
+    if [ "${chroot_path}" == "" ]; then
+        systemctl --user enable --now "$(basename ${systemd_service_file})"
+    fi
 }
 
 function uninstall() {
---
+++ package_managers/ArctisManager.spec
@@ -0,0 +1,46 @@
+Name:           arctis-manager
+Version:        %{_version}
+Release:        1%{?dist}
+Summary:        SteelSeries GG software replacement for Linux to manage Arctis devices
+
+License:        GPL-3.0
+URL:            https://github.com/elegos/Linux-Arctis-Manager
+Source0:        https://github.com/elegos/Linux-Arctis-Manager/archive/refs/tags/v%{version}.zip
+BuildArch:      noarch
+
+# Dipendenze richieste per il pacchetto
+BuildRequires:  bash, wget
+Requires:       bash, python3 >= 3.9, pulseaudio-utils
+
+%description
+SteelSeries GG software replacement to manage standard and advanced Arctis devices features, like ChatMix, ANC, wireless mode etc.
+
+%prep
+echo $(pwd)
+wget https://github.com/elegos/Linux-Arctis-Manager/archive/refs/tags/v%{version}.zip -O v%{version}.zip
+unzip v%{version}.zip
+mv Linux-Arctis-Manager-%{version}/* .
+rm -rf Linux-Arctis-Manager-%{version}
+
+%build
+# Nothing to do here
+
+%install
+# build root cleanup
+rm -rf %{buildroot}
+mkdir -p %{buildroot}
+
+# execute the install script (with chroot feature)
+export CHROOT=%{buildroot}
+export PREFIX="/usr/local"
+bash install.sh
+
+%files
+/usr/local/share/applications/ArctisManager.desktop
+/usr/local/bin/arctis-manager
+/usr/local/lib/arctis-manager
+/usr/lib/udev/rules.d/91-steelseries-arctis.rules
+/usr/lib/systemd/user/arctis-manager.service
+/usr/share/icons/hicolor/scalable/apps/arctis-manager.svg
+
+%changelog
