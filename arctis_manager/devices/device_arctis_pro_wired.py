037f9ddc80e57ed95d5ac834e9e098c3cfa9e00d
mel <1906931+sableeyed@users.noreply.github.com>
2025-10-16 16:14:13 -0500

    udev rules and initial object functions

--- arctis_manager/devices/device_arctis_pro_wired.py
+++ arctis_manager/devices/device_arctis_pro_wired.py
@@ -3,3 +3,44 @@
 # low = c6 00 01
 # medium = c6 80 01
 # high = c6 00 04
+
+# The following data fragments are sent when changing the sidetone from Off -> High
+# Data Fragment: 04400212c600040000000000000000000000000000000000000000000000000000000000000000
+# Data Fragment: 04400111549b000000000000000000000000000000000000000000000000000000000000000000
+# Data Fragment: 2101
+# Data Fragment: 068a4200200900000000000000000000000000000000000000000000000000000000000000
+# Data Fragment: 068a4200200900000000000000000000000000000000000000000000000000000000000000
+# Data Fragment: 06814301230000000000000000000000000000000000000000000000000000000000000000
+# Data Fragment: 06814301220000000000000000000000000000000000000000000000000000000000000000
+
+# The following data fragments are sent when changing the sidetone from High -> Off
+# Data Fragment: 04400212c600000000000000000000000000000000000000000000000000000000000000000000
+# Data Fragment: 04400111549b000000000000000000000000000000000000000000000000000000000000000000
+# Data Fragment: 2101
+# Data Fragment: 068a4200200900000000000000000000000000000000000000000000000000000000000000
+# Data Fragment: 068a4200200900000000000000000000000000000000000000000000000000000000000000
+# Data Fragment: 06814301230000000000000000000000000000000000000000000000000000000000000000
+# Data Fragment: 06814301220000000000000000000000000000000000000000000000000000000000000000
+
+from typing import Optional
+
+from arctis_manager.config_manager import ConfigManager
+from arctis_manager.device_manager import (DeviceState, DeviceManager,
+                                           DeviceStatus, InterfaceEndpoint)
+from arctis_manager.device_manager.device_settings import (DeviceSetting,
+                                                           SliderSetting,
+                                                           ToggleSetting)
+from arctis_manager.device_manager.device_status import DeviceStatusValue
+
+
+class ArctisProWiredDevice(DeviceManager):
+
+    def get_device_name(self):
+        return 'Arctis Pro'
+    
+    def get_device_product_id(self):
+        return 0x1252
+    
+    def get_device_vendor_id(self):
+        return 0x1038
+
