d6a5f805c5beb59493b270a3eb78df9608d9a511
mel <1906931+sableeyed@users.noreply.github.com>
2025-10-16 20:40:14 -0500

    Update device_arctis_pro_wired.py

--- arctis_manager/devices/device_arctis_pro_wired.py
+++ arctis_manager/devices/device_arctis_pro_wired.py
@@ -4,23 +4,14 @@
 # medium = c6 80 01
 # high = c6 00 04
 
-# The following data fragments are sent when changing the sidetone from Off -> High
-# Data Fragment: 04400212c600040000000000000000000000000000000000000000000000000000000000000000
-# Data Fragment: 04400111549b000000000000000000000000000000000000000000000000000000000000000000
-# Data Fragment: 2101
-# Data Fragment: 068a4200200900000000000000000000000000000000000000000000000000000000000000
-# Data Fragment: 068a4200200900000000000000000000000000000000000000000000000000000000000000
-# Data Fragment: 06814301230000000000000000000000000000000000000000000000000000000000000000
-# Data Fragment: 06814301220000000000000000000000000000000000000000000000000000000000000000
-
-# The following data fragments are sent when changing the sidetone from High -> Off
-# Data Fragment: 04400212c600000000000000000000000000000000000000000000000000000000000000000000
-# Data Fragment: 04400111549b000000000000000000000000000000000000000000000000000000000000000000
-# Data Fragment: 2101
-# Data Fragment: 068a4200200900000000000000000000000000000000000000000000000000000000000000
-# Data Fragment: 068a4200200900000000000000000000000000000000000000000000000000000000000000
-# Data Fragment: 06814301230000000000000000000000000000000000000000000000000000000000000000
-# Data Fragment: 06814301220000000000000000000000000000000000000000000000000000000000000000
+# Endpoint 0 = Audio Control
+# Presumably to set the sidetone to low (from off) we need to send the following bytes
+# 0x21 0x09 0x0204 0x005 04400212c600010000000000000000000000000000000000000000000000000000000000000000
+# 0x21 = Host-to-device Request, class 0x1, recipient interface 0x1 (where does this come from -> 0x21 = 00100001)
+# 0x09 = SET_REPORT
+# 0x0204 = ReportID 4, ReportType 2 (output)
+# 0x005 = wIndex
+# wLength always seems to be 39 (includes padding)
 
 from typing import Optional
 
@@ -34,6 +25,16 @@ from arctis_manager.device_manager.device_status import DeviceStatusValue
 
 
 class ArctisProWiredDevice(DeviceManager):
+    game_mix: int = None
+    chat_mix: int = None
+
+    def get_local_settings(self) -> dict[str, int]:
+
+        self._local_config = ConfigManager.get_instance().get_config(self.get_device_vendor_id(), self.get_device_product_id()) or {
+            'mic_side_tone': 0x00, #off
+        }
+
+        return self._local_config
 
     def get_device_name(self):
         return 'Arctis Pro'
